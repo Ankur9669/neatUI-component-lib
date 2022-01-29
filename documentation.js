f1009094fa7133b0160a96ce722e829d37770900
Ankur Gupta <65125998+Ankur9669@users.noreply.github.com>
2022-01-29 16:12:18 +0530

    styled the site and added buttons and badges (#3)
    1. Added Buttons and Batches
    2. Improved the problem of navbar now it looks good in bigger screens.
    3. Converted all the units given in px to rem in documentation.css.
    4. Added scrolling functionality in documentation.js.

---
+++ documentation.js
@@ -0,0 +1,38 @@
+let alerts = document.getElementById("alerts");
+let avatar = document.getElementById("avatar");
+let badges = document.getElementById("badges");
+let buttons = document.getElementById("buttons");
+
+//
+let alertsSection = document.getElementById("alerts-section");
+let avatarSection = document.getElementById("avatar-section");
+let badgesSection = document.getElementById("badges-section");
+let buttonsSection = document.getElementById("buttons-section");
+//
+
+let yOffset = -100;
+alerts.addEventListener("click", () => {
+  let y =
+    alertsSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  scrollToView(y);
+});
+avatar.addEventListener("click", () => {
+  let y =
+    avatarSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  scrollToView(y);
+});
+badges.addEventListener("click", () => {
+  let y =
+    badgesSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  scrollToView(y);
+});
+buttons.addEventListener("click", () => {
+  let y =
+    buttonsSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  scrollToView(y);
+});
+
+// Function to enable scrolling
+function scrollToView(offsetY) {
+  window.scrollTo({ top: offsetY, behavior: "smooth" });
+}
